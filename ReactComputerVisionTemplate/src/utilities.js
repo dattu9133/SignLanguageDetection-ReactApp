103956fde0f059f285a416996743432915ce222e
dattu9133 <dattasaimoganti@gmail.com>
2024-06-28 19:18:37 +0530

    Add react web app for sign language detection

---
+++ ReactComputerVisionTemplate/src/utilities.js
@@ -0,0 +1,36 @@
+// Define our labelmap
+export const labelMap = {
+    1: { name: 'Hello', color: 'red' },
+    2: { name: 'Yes', color: 'blue' },
+    3: { name: 'No', color: 'purple' },
+    4: { name: 'Thank you', color: 'yellow' },
+    5: { name: 'I love you', color: 'lime' },
+    6: { name: 'Eat', color: 'orange' },
+    7: { name: 'Please', color: 'cyan' },
+    8: { name: 'Help', color: 'magenta' },
+    9: { name: 'Nice', color: 'pink' },
+    10: { name: 'More', color: 'green' }
+};
+
+// Define a drawing function
+export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight, ctx) => {
+    for (let i = 0; i < boxes.length; i++) {
+        if (boxes[i] && classes[i] && scores[i] > threshold) {
+            // Extract variables
+            const [y, x, height, width] = boxes[i]
+            const text = classes[i]
+
+            // Set styling
+            ctx.strokeStyle = labelMap[text].color;
+            ctx.lineWidth = 10;
+            ctx.fillStyle = 'white';
+            ctx.font = '30px Arial';
+
+            // DRAW!!
+            ctx.beginPath();
+            ctx.fillText(labelMap[text].name + ' - ' + Math.round(scores[i] * 100) / 100, x * imgWidth, y * imgHeight - 10);
+            ctx.rect(x * imgWidth, y * imgHeight, width * imgWidth / 2, height * imgHeight / 1.5);
+            ctx.stroke();
+        }
+    }
+};
